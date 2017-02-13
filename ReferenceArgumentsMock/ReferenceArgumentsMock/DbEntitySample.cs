75ca91fb6f21ae1519add9895873ef5021bd4a79
Łukasz Księżak <lukasz.ksiezak@interia.pl>
2017-02-13 18:55:22 +0100

    Out argument mock with nice abstraction layer

---
+++ ReferenceArgumentsMock/ReferenceArgumentsMock/DbEntitySample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceArgumentsMock
+{
+    public class DbEntityFactory<T> : IEntityContract<T> where T: class, new()
+    {
+        public void CreateEntity(out T entityToName)
+        {
+            entityToName = new T();
+        }
+    }
+    class DbEntitySample 
+    {
+        protected DbEntitySample()
+        {
+            this.Guid = new Guid();
+        }
+        protected virtual Guid Guid { get; set; }      
+    }
+    interface IEntityContract<T> where T: class, new()
+    {
+        void CreateEntity(out T entityToName);
+    }
+}
---
+++ ReferenceArgumentsMock/ReferenceArgumentsMockTests/DbEntitySampleTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReferenceArgumentsMock;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+
+namespace ReferenceArgumentsMock.Tests
+{
+    public class DbEntityFake: DbEntitySampleTests
+    {
+        public Guid Guid { get; set; }
+    }
+
+    [TestClass()]
+    public class DbEntitySampleTests
+    {
+        [TestMethod()]
+        public void CreateEntityAndAssignNameTest()
+        {
+            //Arrange
+            var mockEntityFactory = Substitute.For<DbEntityFactory<DbEntityFake>>();
+            DbEntityFake fakeEntity;
+            //Act
+            mockEntityFactory.CreateEntity(out fakeEntity);
+            //Assert
+            Assert.IsNotNull(fakeEntity);
+            Assert.IsNotNull(fakeEntity.Guid);
+        }
+    }
+}
